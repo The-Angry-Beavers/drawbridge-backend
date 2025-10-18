272d29a7559975b327644c7e945196b7e25e0147
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2025-10-18 20:32:37 +0300

    wip: add choices as model

--- drawbridge_backend/db/models/tables.py
+++ drawbridge_backend/db/models/tables.py
@@ -29,11 +29,13 @@ class TableModel(Base):
     verbose_name: Mapped[str] = mapped_column(String(256), nullable=False)
     description: Mapped[str | None]
     namespace_id: Mapped[int] = mapped_column(
-        ForeignKey("namespaces.id"), nullable=True,
+        ForeignKey("namespaces.id"),
+        nullable=True,
     )
 
     fields: Mapped[list["FieldModel"]] = relationship(
-        back_populates="table", cascade="all, delete-orphan",
+        back_populates="table",
+        cascade="all, delete-orphan",
     )
     namespace: Mapped["NameSpaceModel"] = relationship(back_populates="tables")
 
@@ -48,9 +50,22 @@ class FieldModel(Base):
     name: Mapped[str] = mapped_column(String(256), nullable=False)
     verbose_name: Mapped[str] = mapped_column(String(256), nullable=False)
     data_type: Mapped[DataTypeEnum] = mapped_column(
-        Enum(DataTypeEnum, native_enum=False), nullable=False,
+        Enum(DataTypeEnum, native_enum=False),
+        nullable=False,
     )
 
     is_nullable: Mapped[bool] = mapped_column(nullable=False, default=True)
     default_value: Mapped[str | None] = mapped_column(String(256), nullable=True)
     table: Mapped["TableModel"] = relationship(back_populates="fields")
+
+
+class FieldChoices(Base):
+    """
+    Base class for all field choices models.
+    """
+
+    __tablename__ = "field_choices"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    field_id: Mapped[int] = mapped_column(ForeignKey("fields.id"), nullable=False)
+    value: Mapped[str] = mapped_column(String(256), nullable=False)
