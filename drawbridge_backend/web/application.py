a8c6d6daf0322c99b79ac91c816dc874579322ce
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2025-10-18 21:15:59 +0300

    fix: setup CORSES

--- drawbridge_backend/web/application.py
+++ drawbridge_backend/web/application.py
@@ -29,7 +29,7 @@ def get_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            "http://localhost:5173" "http://localhost:5174",
+            "http://localhost:5173", "http://localhost:5174",
         ],
         allow_credentials=True,
         allow_methods=["*"],
