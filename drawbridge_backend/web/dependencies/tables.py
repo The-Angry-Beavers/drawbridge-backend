ca96fd9e732e976f2b3b0834a42613a1d3a81bdb
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2025-10-18 15:41:26 +0300

    wip: implement di func fo table service

--- drawbridge_backend/web/dependencies/tables.py
+++ drawbridge_backend/web/dependencies/tables.py
@@ -1,8 +1,23 @@
-from fastapi.params import Depends
+from typing import Annotated
 
-from drawbridge_backend.db.dependencies import get_storage_db_engine
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
+
+from drawbridge_backend.db.dependencies import (
+    get_storage_db_engine,
+    get_storage_db_session,
+    get_db_session,
+)
 from drawbridge_backend.domain.impl.tables import SqlAlchemyTablesService
 
 
-def get_tables_service() -> SqlAlchemyTablesService:
-    raise NotImplementedError()
+def get_tables_service(
+    storage_db_engine: Annotated[AsyncEngine, Depends(get_storage_db_engine)],
+    storage_db_session: Annotated[AsyncSession, Depends(get_storage_db_session)],
+    db_session: Annotated[AsyncSession, Depends(get_db_session)],
+) -> SqlAlchemyTablesService:
+    return SqlAlchemyTablesService(
+        db_session,
+        storage_db_session,
+        storage_db_engine,
+    )
